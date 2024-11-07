d3f7d410ac0f04eb83928f41aadf0ffe316544a4
aherrert <st2020215@365.stucom.com>
2024-11-07 19:56:03 +0100

    commit signup changes

--- php/mysqlSignUp.php
+++ php/mysqlSignUp.php
@@ -3,7 +3,7 @@ $message = "";
 
 // Establecemos la conexión con la base de datos
 if (!empty($_POST)) {
-    $link = mysqli_connect("10.30.212.71","grupo7","Pa$$w0rd","pokewebapp");
+    $link = mysqli_connect('10.30.212.71','grupo7','Pa$$w0rd','pokewebapp');
     // Revisamos que se haya realizado la conexión
     if ($link == false){
         die("ERROR: Could not connect ".mysqli_connect_error());
@@ -16,7 +16,7 @@ if (!empty($_POST)) {
         $date = date("Y/m/d h:i:s");
         
         // Preparamos el query para evitar inyecciones SQL
-        $stmt = $link->prepare("SELECT * FROM Usuario WHERE correo=?");
+        $stmt = $link->prepare("SELECT * FROM usuario WHERE correo=?");
         $stmt->bind_param("s", $email);
         $stmt->execute();
         $result = $stmt->get_result();
@@ -24,11 +24,11 @@ if (!empty($_POST)) {
         if ($result->num_rows > 0){
             $message = "Error: Email already exists";
         } else {
-            $stmt = $link->prepare("INSERT INTO Usuario (nombre,correo,contrasena,pokeballs,fecha_creacion) VALUES (?, ?, ?, ?, ?)");
+            $stmt = $link->prepare("INSERT INTO usuario (nombre,correo,contrasena,pokeballs,fecha_creacion) VALUES (?, ?, ?, ?, ?)");
             $stmt->bind_param("sssis", $name, $email, $pwd, $pokeballs, $date);
             $stmt->execute();
             
-            $stmt = $link->prepare("SELECT id FROM Usuario WHERE correo=?");
+            $stmt = $link->prepare("SELECT id FROM usuario WHERE correo=?");
             $stmt->bind_param("s", $email);
             $stmt->execute();
             $result = $stmt->get_result();
